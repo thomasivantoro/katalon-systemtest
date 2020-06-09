acfd71b5cb95c07d8a6be3298be9975707ba85b5
thom <thomas.ivantoro@naradacode.com>
2020-06-09 15:38:52 +0700

    initial add

---
+++ Test Suites/TS - Login Smoke Test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Login Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>56dbb2cf-19c3-4099-998d-f2a5c48fd188</testSuiteGuid>
+   <testCaseLink>
+      <guid>66fdc42b-27a1-4c90-a35f-33c0994236b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Security/TC - Login Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
